a7736dd45fbe1a32a55f855f2a056f89c4b38ac1
Michael Raichelson <i.am@michaelraichelson.com>
2022-03-01 19:48:53 -0600

    Adjust how the serverless function is structured

--- functions/ip.js
+++ functions/ip.js
@@ -1,19 +1,20 @@
 /**
  * Returns the IP address for this request.
  */
-exports.handler = (event, context, callback) => {
-  callback(null, {
+
+exports.handler = async (event) => {
+  return {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Headers':
-        'Origin, X-Requested-With, Content-Type, Accept'
+        'Origin, X-Requested-With, Content-Type, Accept',
     },
     body: JSON.stringify({
       ip:
         event.headers['client-ip'] !== '::1'
           ? event.headers['x-nf-client-connection-ip']
-          : '127.0.0.1'
-    })
-  })
+          : '127.0.0.1',
+    }),
+  }
 }
